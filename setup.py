cdf855614a6f6af26eac191c214d5859482eec5d
Jens Nistler <jens.nistler@regiohelden.de>
2013-01-09 10:58:40 +0100

    set version to 1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     ],
     packages = find_packages('.'),
     package_dir = {'':'.'},
-    version = "1.0.1",
+    version = "1.1",
     description = "Timeout a logged user after a period of time",
     long_description=open('README.md').read(-1),
     author = "Tomas Zulberti",
