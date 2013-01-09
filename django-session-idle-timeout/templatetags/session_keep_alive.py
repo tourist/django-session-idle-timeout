c67bca370af2425a74c522429d6c286256391356
Jens Nistler <jens.nistler@regiohelden.de>
2013-01-09 15:28:18 +0100

    change template folder, change templatetag definition, bump to 1.1.3

--- django-session-idle-timeout/templatetags/session_keep_alive.py
+++ django-session-idle-timeout/templatetags/session_keep_alive.py
@@ -3,9 +3,8 @@ from django import template
 from django.conf import settings
 
 register = template.Library()
-
+@register.inclusion_tag('sessionidletimeout/js.html', takes_context=True)
 def session_keep_alive(context):
     return context.update({
         'interval': int(getattr(settings, 'SESSION_IDLE_TIMEOUT', 1800)) / 2 * 1000,
     })
-register.inclusion_tag('session-idle-timeout/js.html', takes_context=True)(session_keep_alive)
