7100d87fc5b47b061dc0bb5ccad140fab10696c2
Michał Oleniec <michal.oleniec@apptimia.com>
2023-09-08 16:58:20 +0200

    fix for newer releases of django
    based on: https://github.com/tzulberti/django-session-idle-timeout/pull/7

--- django-session-idle-timeout/urls.py
+++ django-session-idle-timeout/urls.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
 
-from django.conf.urls import patterns, url
-import views
+from django.conf.urls import url
+from . import views
 
-urlpatterns = patterns('',
-    url(r"^/?", views.PingView.as_view(), name="django-session-idle-timeout_ping"),
-)
+urlpatterns = [
+    url(r"^$", views.PingView.as_view(), name="django-session-idle-timeout_ping"),
+]
